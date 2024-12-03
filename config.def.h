e5c83b84824dedb130af2d6619a34131f4391403
violetjewel <violetjewel1729@gmail.com>
2024-12-03 16:28:36 -0500

    add my changes
    tl;dr:
    - tweak numbers
    - theme
    - better rules
    - Mod4Mask
    - tweak default keybindings
    - my keybindings
    
    TODO: update README later when I have time

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 8;        /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -10,37 +10,47 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true" };
+static const char dmenufont[]       = "FiraCode Nerd Font:pixelsize=16:antialias=true:autohint=true";
+
+struct theme {
+	const char *fg;
+	const char *bg;
+	const char *active;
+	const char *active2;
+	const char *inactive;
+};
+
+const struct theme theme = { // tokyonight
+	"#c8d3f5", "#222436",
+	"#c099ff", "#fca7ea", "#636da6", };
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { theme.fg, theme.bg,     theme.inactive },
+	[SchemeSel]  = { theme.bg, theme.active, theme.active2  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { ">_", "www", ">_", ">_", ">_", ">_", ">_", ">_", "chat" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// /* class                 instance    title       tags mask     isfloating   monitor */
+	// { "Gimp",                NULL,       NULL,       0,            1,           -1 },
+	{ "mercury-default",        NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "discord",                NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "irc",                    NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5;  /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -50,7 +60,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -59,24 +69,26 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+static const char term[] = "st";
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", theme.bg, "-nf", theme.fg, "-sb", theme.active, "-sf", theme.bg, NULL };
+static const char *termcmd[]  = { term, NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.02} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.02} },
+	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = 50 } },
+	{ MODKEY|ShiftMask,             XK_m,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -100,6 +112,23 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	{ MODKEY|Mod1Mask,              XK_u,      spawn,          SHCMD("xbacklight -set 1"  ) },
+	{ MODKEY|Mod1Mask,              XK_i,      spawn,          SHCMD("xbacklight -dec 5"  ) },
+	{ MODKEY|Mod1Mask,              XK_o,      spawn,          SHCMD("xbacklight -inc 5"  ) },
+	{ MODKEY|Mod1Mask,              XK_p,      spawn,          SHCMD("xbacklight -set 100") },
+
+	{ MODKEY|Mod1Mask,              XK_h,      spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle;") },
+	{ MODKEY|Mod1Mask,              XK_j,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%;") },
+	{ MODKEY|Mod1Mask,              XK_k,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%;") },
+	{ MODKEY|Mod1Mask,              XK_l,      spawn,          SHCMD("notify-send -h 'string:synchronous:volume' \"$(pactl get-sink-volume '@DEFAULT_SINK@' | head -n1 | awk '{ printf \"%s | %s\", $5, $12 }';[ \"$(pactl get-sink-mute '@DEFAULT_SINK@' | cut -d' ' -f2)\" = 'yes' ] && echo -n ' (muted)')\"") },
+
+	{ MODKEY,                       XK_s,      spawn,          SHCMD("ekraneto plena") },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("ekraneto selektu") },
+	{ MODKEY|ControlMask,           XK_s,      spawn,          SHCMD("ekraneto fenestro") },
+
+	{ MODKEY,                       XK_w,      spawn,          {.v = (const char*[]){ "firefox", NULL } } },
+	{ MODKEY|ShiftMask,             XK_i,      spawn,          {.v = (const char*[]){ term, "-c", "irc", "-e", "weechat", NULL } } },
 };
 
 /* button definitions */
